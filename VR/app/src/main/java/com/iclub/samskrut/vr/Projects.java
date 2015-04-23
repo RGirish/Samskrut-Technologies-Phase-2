6bb17d8f11e30a4255d519cae7745bce55b0bf14
Girish <rgirish1994@gmail.com>
2015-04-23 19:13:32 +0530

    omniP first commit

--- VR/app/src/main/java/com/iclub/samskrut/vr/Projects.java
+++ VR/app/src/main/java/com/iclub/samskrut/vr/Projects.java
@@ -1,17 +1,53 @@
 package com.iclub.samskrut.vr;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.Eye;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
 
-public class Projects extends ActionBarActivity {
+import javax.microedition.khronos.egl.EGLConfig;
+
+public class Projects extends CardboardActivity implements CardboardView.StereoRenderer{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_projects);
+        CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        cardboardView.setRenderer(this);
+        setCardboardView(cardboardView);
     }
 
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+
+    }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int i, int i1) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
+    }
 }
